f1a9a7fdf916cf7f13d999d47989dc4bc70de8c1
n <2185802@cegepmontpetit.ca>
2024-10-08 13:11:01 -0400

    FCT Reactive Form

--- Intra_Formatif_H24-main/question3/src/app/app.component.ts
+++ Intra_Formatif_H24-main/question3/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, ValidationErrors, Validators } from '@angular/forms';
+import { AbstractControl, Form, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Data } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,41 @@ import { AbstractControl, FormBuilder, ValidationErrors, Validators } from '@ang
 export class AppComponent {
   title = 'reactive.form';
 
-  
+  form: FormGroup<any>;
+  formData? : Data;
 
-  constructor(
+  constructor(private fb:FormBuilder
     
-  ) { }
+  ) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required]],
+      roadnumber: ['', [Validators.required]],
+      postalcode: ['', [Validators.pattern("^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$")]]
+    }, {validator: [this.roadnumberValidator]})
+   }
+
+
+   
+  roadnumberValidator(form: AbstractControl): ValidationErrors | null {
+    const roadNumber = form.get('roadnumber')?.value;
+    console.log(roadNumber);
+    console.log("Allo");
+
+    if(!roadNumber){
+      return null
+    }
+  
+    if(roadNumber < 1000){
+      console.log("error");
+      form.get('roadnumber')?.setErrors({tropPetit:true});
+    }
+    else{
+      form.get('roadnumber')?.setErrors(null);
+    }
+    
+   
+    return null
+  }
 }
 
 
